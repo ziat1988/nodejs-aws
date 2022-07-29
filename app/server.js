ed2350cfb518f47071d50ee09f87cedce7af7169
dang <trinhdangnhat@gmail.com>
2022-07-30 01:27:27 +0200

    reset server.js

--- app/server.js
+++ app/server.js
@@ -1,52 +1,21 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
 
-const { mongoConnect } = require("./services/mongo");
 const app = express();
 
-app.use(express.json()); // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
-app.use("/asset", express.static(__dirname + "/public"));
-
-async function getData() {
-  const { db } = mongoose.connection;
-  const items = await db.collection("users").find({}).toArray();
-  return items;
-}
-
-async function saveUser(data) {
-  const { db } = mongoose.connection;
-  await db.collection("users").insertOne(data);
-  return "ok";
-}
-
 app.get("/", (req, res) => {
-  getData().then((data) => {
-    res.send(data);
+  console.log(process.env);
+  res.send("This is some text22");
+});
+
+app.get("/home", (req, res) => {
+  res.send({
+    name: "dang",
+    age: 34,
   });
 });
-
-app.get("/add", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-app.post("/post-data", (req, res) => {
-  saveUser(req.body).then((data) => {
-    console.log(data);
-    res.redirect("/"); // redirect home page
-  });
-});
-
-app.get("/favicon.ico", (req, res) => res.status(204));
 
 const PORT = process.env.PORT || 8000;
 
-async function startServer() {
-  await mongoConnect();
-  app.listen(PORT, function () {
-    console.log(`server run in port ${PORT}`);
-  });
-}
-
-startServer();
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
