a8b0d9e4a8d3ffd595a9b5db2d33a4bfb54e8116
dang <trinhdangnhat@gmail.com>
2022-07-30 01:32:19 +0200

    small change

--- app/server.js
+++ app/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
+const { mongoConnect } = require("./services/mongo");
 const app = express();
+
+app.use(express.json()); // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+app.use("/asset", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   console.log(process.env);
