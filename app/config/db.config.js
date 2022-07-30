a81e2d1ef95d132047b4fb4c8372bcbf8ea13976
dang <trinhdangnhat@gmail.com>
2022-07-30 14:52:53 +0200

    reee

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 //const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 module.exports = {
-  url: `mongodb://admin:password@mongodb:27017/my-test?authSource=admin`,
+  url: "mongodb://admin:password@mongodb:27017/my-test?authSource=admin",
 };
