2c7be4a1d5554ff18a40c4a182edb15917da0597
dang <trinhdangnhat@gmail.com>
2022-07-30 14:27:54 +0200

    whatttt

--- app/server.js
+++ app/server.js
@@ -9,22 +9,8 @@ app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 app.use("/asset", express.static(__dirname + "/public"));
 
-async function getData() {
-  const { db } = mongoose.connection;
-  const items = await db.collection("users").find({}).toArray();
-  return items;
-}
-
-async function saveUser(data) {
-  const { db } = mongoose.connection;
-  await db.collection("users").insertOne(data);
-  return "ok";
-}
-
 app.get("/", (req, res) => {
-  getData().then((data) => {
-    res.send(data);
-  });
+  res.send(process.env); // spacing level = 2(process.env));
 });
 
 app.get("/home", (req, res) => {
@@ -34,25 +20,12 @@ app.get("/home", (req, res) => {
   });
 });
 
-app.get("/add", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-app.post("/post-data", (req, res) => {
-  saveUser(req.body).then((data) => {
-    console.log(data);
-    res.redirect("/"); // redirect home page
-  });
-});
-
-app.get("/favicon.ico", (req, res) => res.status(204));
-
 const PORT = process.env.PORT || 8000;
 
 async function startServer() {
   await mongoConnect();
   app.listen(PORT, function () {
-    console.log(`server run in port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
   });
 }
 
